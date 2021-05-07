187db00c4d5ddc000c62a98c983d4c4b477f70a4
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-05-07 11:13:18 -0600

    pass  test 1 for constructor

--- PierreBakery.Tests/ModelTests/ItemTests.cs
+++ PierreBakery.Tests/ModelTests/ItemTests.cs
@@ -6,19 +6,19 @@ using System.Collections.Generic;
 namespace PierreBakery.Tests
 {
   [TestClass]
-  public class ItemsTests   //: IDisposable
+  public class ItemsTests //: IDisposable
   {
-    public void Dispose()
-  //   {
-  //   Item.ClearAll();
-  //   }
-  //   [TestMethod]
-  //   public void ItemConstructor_CreatesInstanceOfItem_Item()
-  //   {
-  //     Item newItem = new Item();
-  //     Assert.AreEqual(typeof(Item), newItem.GetType());
-  //   }
-  // }
+    // public void Dispose()
+    // {
+    // Item.ClearAll();
+    // }
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item();
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+  }
 }
 
 
