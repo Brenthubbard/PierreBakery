574edeb6502b2e245fa986ed63b31f96743e3544
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-05-07 12:24:16 -0600

    add Program.cs and WIP testing

--- PierreBakery.Tests/ModelTests/BakeryTests.cs
+++ PierreBakery.Tests/ModelTests/BakeryTests.cs
@@ -18,6 +18,19 @@ namespace PierreBakery.Tests
       Item newItem = new Item();
       Assert.AreEqual(typeof(Item), newItem.GetType());
     }
+    [TestMethod]
+    public void GetOrderDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      String description = "UserInput";
+      Item newItem = new Item(description);
+
+      //Act
+      string result = newItem.Description;
+
+      //Assert
+      Assert.AreEqual(description, result);
+    }
   }
 }
 
