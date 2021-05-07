b80cb3107089e5c483b2ae12b9426b792caf8bd9
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-05-07 17:42:31 -0600

    WIP fixing errors

--- PierreBakery.Tests/ModelTests/BakeryTests.cs
+++ PierreBakery.Tests/ModelTests/BakeryTests.cs
@@ -6,12 +6,9 @@ using PierreBakery.Models;
 namespace PierreBakery.Tests
 {
   [TestClass]
-  public class BakeryTests //: IDisposable
+  public class BakeryTests 
   {
-    // public void Dispose()
-    // {
-    // Item.ClearAll();
-    // }
+    
     [TestMethod]
     public void LoafConstructor_CreatesInstanceOfLoaf_Loaf()
     {
@@ -25,7 +22,28 @@ namespace PierreBakery.Tests
       Loaf newLoafOrder = new Loaf(loafOrder);
       int result = newLoafOrder.LoafOrderPrice();
       Assert.AreEqual(5,result);
-    // }
+    }
+    [TestMethod]
+    public void LoafOrderPrice_ReturnsPriceFor2Loaf_int()
+    {
+      int loafOrder = 2;
+      Loaf newLoafOrder = new Loaf(loafOrder);
+      int result = newLoafOrder.LoafOrderPrice();
+      Assert.AreEqual(10, result);
+    }
+    [TestMethod]
+    public void PastryConstructor_CreateInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry();
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType);
+    }
+    [TestMethod]
+    public void PastryOrderPrice_ReturnsPriceFor1Pastry_int()
+    {
+      int pastryOrder = 1;
+      Pastry newPastryOrder = new Pastry(pastryOrder);
+      int result = newPastryOrder.PastryOrderPrice();
+      Assert.AreEqual(2, result);
     }
   }
 }
