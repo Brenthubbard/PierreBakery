79deb7004b63d9f545aec122da47b58fecf26e84
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-05-07 15:03:43 -0600

    Loaf constructer creating instance of Loaf

--- PierreBakery.Tests/ModelTests/BakeryTests.cs
+++ PierreBakery.Tests/ModelTests/BakeryTests.cs
@@ -1,36 +1,24 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PierreBakery.Models;
 using System.Collections.Generic;
+using PierreBakery.Models;
 
 namespace PierreBakery.Tests
 {
   [TestClass]
-  public class ItemsTests //: IDisposable
+  public class BakeryTests //: IDisposable
   {
     // public void Dispose()
     // {
     // Item.ClearAll();
     // }
     [TestMethod]
-    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    public void LoafConstructor_CreatesInstanceOfLoaf_Loaf()
     {
-      Item newItem = new Item();
-      Assert.AreEqual(typeof(Item), newItem.GetType());
+      Loaf newLoaf = new Loaf(1);
+      Assert.AreEqual(typeof(Loaf), newLoaf.GetType());
     }
-    [TestMethod]
-    public void GetOrderDescription_ReturnsDescription_String()
-    {
-      //Arrange
-      String description = "UserInput";
-      Item newItem = new Item(description);
-
-      //Act
-      string result = newItem.Description;
-
-      //Assert
-      Assert.AreEqual(description, result);
-    }
+    
   }
 }
 
