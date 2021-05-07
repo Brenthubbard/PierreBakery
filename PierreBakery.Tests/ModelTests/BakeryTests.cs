ed56bd6b4068e7d2742d453008787c1cdf65c14d
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-05-07 16:05:03 -0600

    update readme

--- PierreBakery.Tests/ModelTests/BakeryTests.cs
+++ PierreBakery.Tests/ModelTests/BakeryTests.cs
@@ -18,7 +18,14 @@ namespace PierreBakery.Tests
       Loaf newLoaf = new Loaf();
       Assert.AreEqual(typeof(Loaf), newLoaf.GetType());
     }
-    
+    [TestMethod]
+    public void LoafOrderPrice_ReturnsPriceFor1Loaf_int()
+    {
+      int loafOrder = 1;
+      Loaf newLoafOrder = new Loaf(loafOrder);
+      int result = newLoafOrder.LoafOrderPrice();
+      Assert.AreEqual(5,result);
+    // }
   }
 }
 
